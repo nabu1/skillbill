626b97cd5afced09ee3457ab0d1848fb8d24682e
nabu1 <nabu1312@gmail.com>
2019-02-25 10:33:00 +0100

    WiP Table - filtry

--- src/components/Table/Table.vue
+++ src/components/Table/Table.vue
@@ -9,13 +9,13 @@ export default {
       { text: 'id', sortable: true, value: 'id' },
       { text: 'title', sortable: true, value: 'title' },
       { text: 'Name', sortable: false, value: 'first' },
-      { text: 'City', sortable: true, value: 'city' },
       { text: 'Skill 1', sortable: true, value: 'skill_1' },
       { text: 'Rank 1', sortable: false, value: 'rank_1' },
       { text: 'Skill 2', sortable: true, value: 'rank_2' },
       { text: 'Rank 2', sortable: false, value: 'rank_2' },
       { text: 'Skill 3', sortable: true, value: 'rank_3' },
       { text: 'Rank 3', sortable: false, value: 'rank_3' },
+      { text: 'City', sortable: true, value: 'city' },
       { text: 'Picture', sortable: true, value: 'picture' }
     ],
     devs: [],
@@ -112,9 +112,7 @@ export default {
 <style scoped>
 
   .filters {
-    /* background-color: #424242; */
-   /*  background-color: gray; */
-    transform: scale(0.55, 1);
+    transform: scale(0.8, 1);
     transform-origin: left;
   }
 
