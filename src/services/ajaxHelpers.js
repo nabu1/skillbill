2b45777651c390edca9a252283728f7695c21190
nabu1 <nabu1312@gmail.com>
2019-02-25 17:35:38 +0100

    WiP http query ok

--- src/services/ajaxHelpers.js
+++ src/services/ajaxHelpers.js
@@ -5,7 +5,10 @@ export default (context, selectedSkills) => {
 
   // const query = 's={id:1}'
 
-  const query= `q={"skill_1":"C",$and:[{"rank_1":{$gte:5}}]}`
+  // const query = `q={"skill_1":"C",$and:[{"rank_1":{$gte:5}}]}`
+
+  const query = `q={"skill_1":"C",$and:[{"rank_1":{$gte:0}},{$and:[{"skill_2":"Java"},{$and:[{"rank_2":{$gte:0}},{$and:[{"skill_3":"JS"},{$and:[{"rank_3":{$gte:0}}]}]}]}]}]}`
+
 
   const urlString = LITERALS.PREFIX + query + LITERALS.SUFFIX
   console.log('%c urlString =  ' + urlString, 'color: lime')
