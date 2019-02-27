02edf2b36c4644b8d0ce9c4083488e99dae29e89
nabu1 <nabu1312@gmail.com>
2019-02-27 11:47:56 +0100

    Cleaning

--- src/services/ajax.js
+++ src/services/ajax.js
@@ -48,25 +48,11 @@ export const ajaxInsertDev = dev => {
     })
 }
 
-//export const ajaxDelete = _id => {
 export const ajaxDelete = _id => {
   console.log('%c _id = ', 'color: white')
   console.log(_id.replace(/"/g, ''))
 
-  const axios = require('axios')
-  const config = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Z-Key',
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS, HEAD',
-    "Access-Control-Allow-Credentials": "true"
-  }
-  const options = {
-    contentType: "application/json"
-  }
-
   // const url = `https://api.mlab.com/api/1/databases/skillbill/collections/skillbill/5c742d443603900f550d492a?apiKey=XRr-4BkluC11FFgtbOnUhzUlodvp8RfI`
-
   // const url = `https://api.mlab.com/api/1/databases/skillbill/collections/skillbill/${_id.replace(/"/g, '')}?apiKey=XRr-4BkluC11FFgtbOnUhzUlodvp8RfI`
 
   const url = `${LITERALS.PREFIX}/${_id.replace(/"/g, '')}?apiKey=XRr-4BkluC11FFgtbOnUhzUlodvp8RfI`
