2d5f7f0b1f44243dbf403829170d0237e9e1dc84
nabu1 <nabu1312@gmail.com>
2019-02-27 02:23:18 +0100

    Chodzi zapis nowego deva na serwerze

--- src/services/ajax.js
+++ src/services/ajax.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
-import url from './ajaxHelpers'
+import { fetch, insertDev} from './ajaxHelpers'
 
 export const ajaxGetDevs = (context, selectedSkills) => {
   // console.log('%c ajax: selectedSkills = ' + selectedSkills, 'color: white')
 
   axios
-    .get(url(context, selectedSkills))
+    .get(fetch(context, selectedSkills))
     .then(res => {
       //console.log('%c res.data.length = ' + res.data.length, 'color: violet')
 
@@ -18,3 +18,34 @@ export const ajaxGetDevs = (context, selectedSkills) => {
     })
 }
 
+export const ajaxInsertDev = dev => {
+  console.log('%c Tu ajaxInsertDev: dev = ' + dev, 'color: violet')
+  const axios = require('axios')
+  const url = `https://api.mlab.com/api/1/databases/skillbill/collections/skillbill?apiKey=XRr-4BkluC11FFgtbOnUhzUlodvp8RfI`
+
+  axios.defaults.headers.post['Content-Type'] = 'application/json'
+
+  const dev01 = {
+    id: 1001,
+    first: 'Fred',
+    last: 'Flintstone',
+    title: 'Archie',
+    skill_1: 'JS',
+    rank_1: 5,
+    skill_2: 'Java',
+    rank_2: 5,
+    skill_3: 'Go',
+    rank_3: 5
+  }
+
+  //axios.post(url, dev01)
+  axios.post(url, dev)
+    .then(res => {
+      console.log(res)
+      console.log(res.data)
+      alert('Dane zostały zapisane na serwerze')
+    })
+    .catch(err =>  {
+      alert('Błąd zapisu na serwerze: ', err)
+    })
+}
