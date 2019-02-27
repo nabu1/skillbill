a732292d0bafe65740709655d29a404ce3a63789
nabu1 <nabu1312@gmail.com>
2019-02-27 02:59:51 +0100

    Dodanie id do zapisywanego deva

--- src/services/ajax.js
+++ src/services/ajax.js
@@ -19,33 +19,34 @@ export const ajaxGetDevs = (context, selectedSkills) => {
 }
 
 export const ajaxInsertDev = dev => {
-  console.log('%c Tu ajaxInsertDev: dev = ' + dev, 'color: violet')
+  console.log('%c Tu ajaxInsertDev: dev = ' + JSON.stringify(dev), 'color: violet')
   const axios = require('axios')
   const url = `https://api.mlab.com/api/1/databases/skillbill/collections/skillbill?apiKey=XRr-4BkluC11FFgtbOnUhzUlodvp8RfI`
 
   axios.defaults.headers.post['Content-Type'] = 'application/json'
 
-  const dev01 = {
-    id: 1001,
-    first: 'Fred',
-    last: 'Flintstone',
-    title: 'Archie',
-    skill_1: 'JS',
-    rank_1: 5,
-    skill_2: 'Java',
-    rank_2: 5,
-    skill_3: 'Go',
-    rank_3: 5
-  }
+  const countUrl = `https://api.mlab.com/api/1/databases/skillbill/collections/skillbill?&c=true&apiKey=XRr-4BkluC11FFgtbOnUhzUlodvp8RfI`
+
+  axios.get(countUrl)
+    .then(res => {
+      console.log('%c res = ' + res, 'color: blue')
+      dev.id = res.data
+
+      axios.post(url, dev)
+        .then(res => {
+          console.log(res)
+          console.log(res.data)
+          alert('Dane zostały zapisane na serwerze')
+        })
+        .catch(err =>  {
+          alert('Błąd zapisu na serwerze (1): ', err)
+        })
+    })
+    .catch(err => {
+      alert('Błąd zapisu na serwerze (2): ', err)
+    })
+
 
   //axios.post(url, dev01)
-  axios.post(url, dev)
-    .then(res => {
-      console.log(res)
-      console.log(res.data)
-      alert('Dane zostały zapisane na serwerze')
-    })
-    .catch(err =>  {
-      alert('Błąd zapisu na serwerze: ', err)
-    })
+
 }
