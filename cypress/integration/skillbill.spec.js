7b8112e7f39959aa436dfb524164fb54c63f310f
nabu1 <nabu1312@gmail.com>
2019-03-02 19:55:19 +0100

    TDD insertDev

--- cypress/integration/skillbill.spec.js
+++ cypress/integration/skillbill.spec.js
@@ -3,7 +3,7 @@ import { tableCell } from './cyhelper'
 for (let i = 0; i < 1 ; i++) {
 
   describe('I. Documents filtering', () => {
-    it('1. .. using JS=5, Java=3 and ObjC without rank specified, should get at least 3 devs' +
+    it.skip('1. .. using JS=5, Java=3 and ObjC without rank specified, should get at least 3 devs' +
       'and pressing Clear button should clear all the select boxes', () => {
 
       cy.visit('http://localhost:8080')
@@ -75,7 +75,30 @@ for (let i = 0; i < 1 ; i++) {
       cy.get('tr:nth-child(1) > td.text-xs-left.body-2:nth-child(2)').should('not.have.text', '32')
     })
   })
+
+  describe('III. Inserting a dev', () => {
+    it.only('1. ..  should immediately add him to the view', () => {
+      cy.visit('http://localhost:8080').wait(4000)
+
+      cy.get("div.v-select__slot > div.v-select__selections > div").click()
+      cy.get("div:nth-child(9) > a > div > div").click()
+      cy.get("div.v-table__overflow > table > thead > tr:nth-child(1) > th:nth-child(2) > i").click()
+    })
+  })
+
+
+
+
 }
 
+//document.querySelector("#app > div.application--wrap > div > div.elevation-6 > div.v-datatable.v-table.theme--dark > div > div.v-datatable__actions__select > div > div > div > div.v-select__slot > div.v-select__selections > div")
 
-// ("#app > div.application--wrap > div > div.elevation-6 > div.v-table__overflow > table > tbody > tr:nth-child(1) > td.justify-center.layout.px-0 > i:nth-child(2)")
+// ("#app > div.application--wrap > div > div.elevation-6 > div.v-datatable.v-table.theme--dark > div > div.v-datatable__actions__select > div > div > div > div.v-select__slot > div.v-select__selections > div")
+
+// ("#app > div.v-menu__content.theme--dark.v-menu__content--auto.menuable__content__active > div > div > div:nth-child(9) > a > div > div")
+
+//document.querySelector("#app > div.application--wrap > div > div.elevation-6 > div.v-table__overflow > table > thead > tr:nth-child(1) > th.column.sortable.active.desc.text-xs-left")
+
+//("#app > div.application--wrap > div > div.elevation-6 > div.v-table__overflow > table > thead > tr:nth-child(1) > th.column.sortable.active.desc.text-xs-left > i")
+
+//#app > div.application--wrap > div > div.elevation-6 > div.v-table__overflow > table > thead > tr:nth-child(1) > th:nth-child(2) > i
