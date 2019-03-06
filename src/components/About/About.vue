a6bd930eab69a0f91dfc84a3a7d2b49fa77cfcdf
nabu1 <nabu1312@gmail.com>
2019-03-06 21:25:39 +0100

    WiP About

--- src/components/About/About.vue
+++ src/components/About/About.vue
@@ -1,2 +1,20 @@
 <template src="./About.html"></template>
 
+<style scoped>
+  #main {
+    background-color: grey;
+    color: white;
+    font-family: "Roboto";
+    font-size: 1em;
+    margin: 10px 20px 10px 20px;
+    padding: 10px 20px 10px 20px
+  }
+
+  a {
+    color: primary;
+    font-size: 1.4em;
+    font-weight: 400;
+    text-decoration: none;
+  }
+
+</style>
